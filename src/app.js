339daf68745f1ab4e91bdef92a5d83daf73ef52a
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2025-05-13 16:07:30 +0700

    update db

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import productRoutes from "./routes/product.routes.js";
 import supplierRoutes from "./routes/supplier.routes.js";
 import saleRoutes from "./routes/sale.routes.js";
-
+import customerDebtRoutes from "./routes/customerDebt.routes.js";
 const app = express();
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/suppliers", supplierRoutes);
 app.use("/api/sales", saleRoutes);
+app.use("/api/customer-debt", customerDebtRoutes);
+
 export default app;
--- src/db/db.js
+++ src/db/db.js
@@ -8,6 +8,22 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000, // 10 seconds
 });
+
+// Test the connection
+pool
+  .getConnection()
+  .then((connection) => {
+    console.log("Connected to the database");
+    connection.release();
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
 
 export default pool;
