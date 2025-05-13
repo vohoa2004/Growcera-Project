68f7245c7f2770fc76e0e76c2ff25333f7facf9f
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2025-05-13 16:07:22 +0700

    create routes

---
+++ src/routes/product.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getLowStockProducts,
+} from "../controllers/product.controller.js";
+
+const router = Router();
+
+router.get("/", getAllProducts);
+router.get("/low-stock", getLowStockProducts);
+//router.get("/:id", getProductById);
+//router.post("/", createProduct);
+//router.put("/:id", updateProduct);
+//router.delete("/:id", deleteProduct);
+
+export default router;
---
+++ src/routes/sale.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+  getAllSales,
+  getSaleById,
+  createSale,
+  updateSale,
+  deleteSale,
+  getTodayRevenue,
+  getTodayOrderCount,
+  getTopSellingProducts,
+  getMonthlyRevenue,
+} from "../controllers/sale.controller.js";
+
+const router = Router();
+
+router.get("/", getAllSales);
+router.get("/revenue/today", getTodayRevenue);
+router.get("/revenue/month", getMonthlyRevenue);
+router.get("/order-count/today", getTodayOrderCount);
+router.get("/top-products", getTopSellingProducts);
+router.get("/:id", getSaleById);
+router.post("/", createSale);
+router.put("/:id", updateSale);
+router.delete("/:id", deleteSale);
+
+export default router;
