b985621397ca84acea8fc242c76c725c64d3fee5
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2025-05-14 08:51:28 +0700

    add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import productRoutes from "./routes/product.routes.js";
 import supplierRoutes from "./routes/supplier.routes.js";
 import saleRoutes from "./routes/sale.routes.js";
 import customerDebtRoutes from "./routes/customerDebt.routes.js";
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
